c22cf0207e041ce7126251f340ca449375786c1e
Raluca Alecsandra Vlad <120947922+AraluV@users.noreply.github.com>
2024-05-21 17:46:08 +0300

    Delete Facade4 directory

--- Facade4/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Banca banca=new Banca(44,false,false);
-        Facade facade=new Facade(banca);
-        facade.verificareClient();
-    }
-}
