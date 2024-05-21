6a226406c2a9025104f1d958aec95823f9934cab
Raluca Alecsandra Vlad <120947922+AraluV@users.noreply.github.com>
2024-05-21 17:46:28 +0300

    Delete Flyweight2 directory

--- Flyweight2/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Pacient pacient=new Pacient("Mihai","9250980243");
-        Internare i=new Internare(3,4);
-        pacient.afisareInformatii(i);
-
-    }
-}
