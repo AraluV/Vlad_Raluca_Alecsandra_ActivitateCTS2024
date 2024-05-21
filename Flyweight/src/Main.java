5655433f7842564c30351cfce8f2c8eb65dc8dd8
Raluca Alecsandra Vlad <120947922+AraluV@users.noreply.github.com>
2024-05-21 17:46:19 +0300

    Delete Flyweight directory

--- Flyweight/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Banca banca=new Banca("BCR","FERV");
-        Cont cont=new Cont(23,44.5);
-        banca.getClient("Mihai","fervge").afisareDetalii(cont);
-
-
-    }
-}
